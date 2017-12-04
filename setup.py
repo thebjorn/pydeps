8095fed92ebdf58902db0676ef7c798b5e7bb417
Bjorn Pettersen <bp@datakortet.no>
2017-12-04 11:24:04 +0100

    update travis parameters

--- setup.py
+++ setup.py
@@ -1,10 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""pydeps - Python module dependency visualization
+"""
 # pragma: nocover
 import setuptools
 from distutils.core import setup
+from setuptools.command.test import test as TestCommand
 
+version='1.3.8'
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+    
 setup(
     name='pydeps',
-    version='1.3.8',
+    version=version,
     packages=['pydeps'],
     install_requires=[
         'enum34',
