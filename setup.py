a61be34918d8796fe459480fb0e18fa40e3d47a0
eight <eight04@gmail.com>
2018-06-07 11:12:46 +0800

    Fix: don't install enum34 in py3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     version=version,
     packages=['pydeps'],
     install_requires=[
-        'enum34',
+        'enum34; python_version < "3.4"',
         'stdlib_list',
     ],
     long_description=io.open('README.rst', encoding='utf8').read(),
