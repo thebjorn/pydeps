3c7e7d36df39c549c4ce4c737a7f94c8471134e5
Patrick O'Neill <patrick.oneill@kensho.com>
2021-07-29 00:59:08 -0400

    add test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
+import logging
+from unittest.mock import patch, call
 
+from pydeps.cli import error
 from pydeps.pydeps import pydeps
 from tests.filemaker import create_files
 from tests.simpledeps import simpledeps, empty
@@ -24,3 +27,10 @@ def test_output(tmpdir):
         assert not os.path.exists(outname)
         pydeps(fname='foo', **empty('--noshow', output=outname))
         assert os.path.exists(outname)
+
+@patch('sys.exit')
+@patch('builtins.print')
+def test_error(mocked_print, mocked_sys_exit):
+    """Test that error function prints reminder about missing inits on FileNotFoundErrors."""
+    error("[Errno 2] No such file or directory: 'foo'")
+    mocked_print.assert_called_with("\t(Did you forget to include an __init__.py?)")
