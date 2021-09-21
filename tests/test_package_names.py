97621ec5f4827ca26a07377817ce8261048c5667
Bjorn Pettersen <bp@norsktest.no>
2021-09-21 18:37:00 +0200

    Add test for package_names.py

---
+++ tests/test_package_names.py
@@ -0,0 +1,9 @@
+
+
+from pydeps.package_names import find_package_names
+
+
+def test_find_package_names():
+    packages = find_package_names()
+    assert 'pip' in packages
+    assert 'pytest' in packages
