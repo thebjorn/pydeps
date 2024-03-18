608e4147d161fa57978ae25c04b6b66fcd8c0455
thebjorn <bp@datakortet.no>
2024-03-18 15:40:05 +0100

    Hopefully we can rely on pytest being installed when running tests..?  Fixes 1.12.19: pitest fails in `tests/test_package_names.py::test_find_package_names` unit #214

--- tests/test_package_names.py
+++ tests/test_package_names.py
@@ -5,5 +5,5 @@ from pydeps.package_names import find_package_names
 
 def test_find_package_names():
     packages = find_package_names()
-    assert 'pip' in packages
+    # assert 'pip' in packages
     assert 'pytest' in packages
