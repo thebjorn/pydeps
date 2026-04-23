d9ac35521c812d240beb5f93b1fc1a145d993dc5
Bjørn Pettersen <bp@norsktest.no>
2026-04-23 11:33:35 +0200

    Remove deprecated setuptools TestCommand from setup.py
    setuptools.command.test was deprecated and emits warnings on recent setuptools. Nothing in the project invokes 'python setup.py test' — CI and dk testpackage run pytest directly — so just drop the custom cmdclass. Also replaces io.open with the Py3 builtin and drops the now-unused io/sys imports.

--- setup.py
+++ setup.py
@@ -2,32 +2,9 @@
 """pydeps - Python module dependency visualization
 """
 # pragma: nocover
-import io
-import sys
-
 import setuptools
-from setuptools.command.test import test as TestCommand
 
 version='3.0.5'
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 
 setuptools.setup(
@@ -38,14 +15,13 @@ setuptools.setup(
     install_requires=[
         'stdlib_list',
     ],
-    long_description=io.open('README.rst', encoding='utf8').read(),
+    long_description=open('README.rst', encoding='utf8').read(),
     entry_points={
         'console_scripts': [
             'pydeps = pydeps.pydeps:pydeps',
         ]
     },
     url='https://github.com/thebjorn/pydeps',
-    cmdclass={'test': PyTest},
     license='BSD',
     author='bjorn',
     author_email='bp@datakortet.no',
